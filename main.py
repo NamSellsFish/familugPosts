41b73c024392159b98d61ad8c81ba9bdacf6a348
Người Trẻ <nhatnam0192123@gmail.com>
2022-12-09 10:39:38 +0700

    test

---
+++ main.py
@@ -0,0 +1,46 @@
+import sqlite3
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, render_template
+
+# Get Data
+ses = requests.session()
+posts = {"Python": [], "Command": [], "sysadmin": []}
+for articles in posts:
+    ses = requests.get(f"https://www.familug.org/search/label/{articles}")
+    soup = BeautifulSoup(ses.content, "html.parser")
+    posts[articles] = ["{} ({})".format(title.text.strip('\n'), title.find_next().attrs.get('href')) for title in
+                       soup.find_all("h3", class_="post-title entry-title")]
+ses = requests.get("https://familug.github.io/")
+soup = BeautifulSoup(ses.content, "html.parser")
+posts["latest"] = ["{} ({})".format(title.find_next().text.strip('\n  '), title.find_next().attrs.get('href')) for title
+                   in
+                   soup.find_all("div", class_="recent-posts-article")][:10]
+
+# Work with database
+con = sqlite3.connect("POSTS.db", check_same_thread=False)
+cur = con.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS POSTS(No INTERGER,Topic TEXT,details TEXT UNIQUE)")
+for articles in posts:
+    for index, article in enumerate(posts[articles], 1):
+        cur.execute(f"""
+        INSERT OR IGNORE INTO POSTS VALUES
+                {(index, articles, article)}
+        """)
+        con.commit()
+
+# Work with website
+app = Flask("myapp")
+
+
+@app.route("/")
+def showThePost():
+    postsJSON=[]
+    for articles in posts:
+        postsJSON.append(list(cur.execute(f"SELECT * FROM POSTS WHERE Topic = '{articles}' ")))
+    print(postsJSON)
+    return render_template("index.html", posts=postsJSON,)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

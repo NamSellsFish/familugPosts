948651fd795833acd973e25251f2a79c3d61935e
Người Trẻ <110954389+NamSellsFish@users.noreply.github.com>
2022-12-11 09:19:14 +0700

    test

--- main.py
+++ main.py
@@ -9,24 +9,33 @@ posts = {"Python": [], "Command": [], "sysadmin": []}
 for articles in posts:
     ses = requests.get(f"https://www.familug.org/search/label/{articles}")
     soup = BeautifulSoup(ses.content, "html.parser")
-    posts[articles] = ["{} ({})".format(title.text.strip('\n'), title.find_next().attrs.get('href')) for title in
-                       soup.find_all("h3", class_="post-title entry-title")]
+    posts[articles] = [
+        "{} ({})".format(title.text.strip("\n"), title.find_next().attrs.get("href"))
+        for title in soup.find_all("h3", class_="post-title entry-title")
+    ]
 ses = requests.get("https://familug.github.io/")
 soup = BeautifulSoup(ses.content, "html.parser")
-posts["latest"] = ["{} ({})".format(title.find_next().text.strip('\n  '), title.find_next().attrs.get('href')) for title
-                   in
-                   soup.find_all("div", class_="recent-posts-article")][:10]
+posts["latest"] = [
+    "{} ({})".format(
+        title.find_next().text.strip("\n  "), title.find_next().attrs.get("href")
+    )
+    for title in soup.find_all("div", class_="recent-posts-article")
+][:10]
 
 # Work with database
 con = sqlite3.connect("POSTS.db", check_same_thread=False)
 cur = con.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS POSTS(No INTERGER,Topic TEXT,details TEXT UNIQUE)")
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS POSTS(No INTERGER,Topic TEXT,details TEXT UNIQUE)"
+)
 for articles in posts:
     for index, article in enumerate(posts[articles], 1):
-        cur.execute(f"""
+        cur.execute(
+            f"""
         INSERT OR IGNORE INTO POSTS VALUES
                 {(index, articles, article)}
-        """)
+        """
+        )
         con.commit()
 
 # Work with website
@@ -35,11 +44,15 @@ app = Flask("myapp")
 
 @app.route("/")
 def showThePost():
-    postsJSON=[]
+    postsJSON = []
     for articles in posts:
-        postsJSON.append(list(cur.execute(f"SELECT * FROM POSTS WHERE Topic = '{articles}' ")))
-    print(postsJSON)
-    return render_template("index.html", posts=postsJSON,)
+        postsJSON.append(
+            list(cur.execute(f"SELECT * FROM POSTS WHERE Topic = '{articles}' "))
+        )
+    return render_template(
+        "index.html",
+        posts=postsJSON,
+    )
 
 
 if __name__ == "__main__":
